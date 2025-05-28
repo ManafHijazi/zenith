67a841a48f9605c342d0230d14cb9d7adbfdb303
Manaf <mhijazi@first.tech>
2025-05-28 18:59:43 +0300

    force-dynamic fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { supabaseAdmin } from '@/lib/supabase';
 import ServicesGrid from '@/components/ServicesGrid';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const { data: services, error } = await supabaseAdmin.from('services').select('*');
   if (error) {
